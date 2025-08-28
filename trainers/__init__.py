aa6a409875a6764593a8f16e2bb9037c9c378c88
Weishan Li <wli51@jh.edu>
2025-08-28 13:21:28 -0600

    Remove WGANTrainer and Trainer imports: simplify trainer module exports

--- trainers/__init__.py
+++ trainers/__init__.py
@@ -1,9 +1,5 @@
 from .AbstractTrainer import AbstractTrainer
-from .Trainer import Trainer
-from .WGANTrainer import WGANTrainer
 
 __all__ = [
-    'AbstractTrainer',
-    'Trainer',
-    'WGANTrainer',
+    'AbstractTrainer'
 ]
