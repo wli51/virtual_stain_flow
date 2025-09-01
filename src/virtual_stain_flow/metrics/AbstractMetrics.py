7cbbbc7c3dd3b4eff378694aa4931ea849ab235e
Weishan Li <wli51@jh.edu>
2025-09-01 15:49:08 -0600

    Refactor AbstractMetrics to improve metric aggregation, reduce gpu occupation of tensor metric values and enhance some docstring regarding return type and signature.

--- src/virtual_stain_flow/metrics/AbstractMetrics.py
+++ src/virtual_stain_flow/metrics/AbstractMetrics.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Optional
+from typing import Optional, Literal
 
 import torch
 import torch.nn as nn
@@ -39,21 +39,29 @@ class AbstractMetrics(nn.Module, ABC):
         """Computes the metric given information about the data."""
         pass
     
+    @torch.inference_mode()
     def update(self, 
                _generated_outputs: torch.tensor, 
                _targets: torch.tensor, 
                validation: bool=False
                ) -> None:
         """Updates the metric with the new data."""
+
+        val = self.forward(_generated_outputs, _targets)
+
+        if val.numel() > 1:
+            val = val.float().mean()
+        v = float(val.detach().item())
+
         if validation:
-            self.__val_metric_values.append(self.forward(_generated_outputs, _targets))
+            self.__val_metric_values.append(v)
         else:
-            self.__train_metric_values.append(self.forward(_generated_outputs, _targets))
+            self.__train_metric_values.append(v)
 
     def reset(self):
         """Resets the metric."""
-        self.__train_metric_values = []
-        self.__val_metric_values = []
+        self.__train_metric_values.clear()
+        self.__val_metric_values.clear()
 
     def compute(self, **kwargs):
         """
@@ -62,30 +70,36 @@ class AbstractMetrics(nn.Module, ABC):
         """
         return self.aggregate_metrics(**kwargs)
 
-    def aggregate_metrics(self, aggregation: Optional[str] = 'mean'):
+    def aggregate_metrics(
+        self, 
+        aggregation: Optional[Literal['mean', 'sum']]='mean'
+    ) -> tuple[Optional[torch.tensor], Optional[torch.tensor]]:
         """
-        Aggregates the metric value over batches
+        Aggregates the metric value over batches. Returns the training and
+            validation metric values.
 
         :param aggregation: The aggregation method to use, by default 'mean'
         :type aggregation: Optional[str]
         :return: The aggregated metric value for training and validation
-        :rtype: Tuple[torch.tensor, torch.tensor]
         """
 
+        tr_list, va_list = self.__train_metric_values, self.__val_metric_values
+
         if aggregation == 'mean':
-            return \
-            torch.mean(torch.stack(self.__train_metric_values)) if len(self.__train_metric_values) > 0 else None , \
-            torch.mean(torch.stack(self.__val_metric_values)) if len(self.__val_metric_values) > 0 else None
+            tr = torch.tensor(sum(tr_list)/len(tr_list)) if tr_list else None
+            va = torch.tensor(sum(va_list)/len(va_list)) if va_list else None
+            return tr, va
         
         elif aggregation == 'sum':
-            return \
-            torch.sum(torch.stack(self.__train_metric_values)) if len(self.__train_metric_values) > 0 else None , \
-            torch.sum(torch.stack(self.__val_metric_values)) if len(self.__val_metric_values) > 0 else None
+            tr = torch.tensor(sum(tr_list)) if tr_list else None
+            va = torch.tensor(sum(va_list)) if va_list else None
+            return tr, va
         
         elif aggregation is None:
-            return \
-            torch.stack(self.__train_metric_values) if len(self.__train_metric_values) > 0 else None , \
-            torch.stack(self.__val_metric_values) if len(self.__val_metric_values) > 0 else None
+            tr = torch.tensor(tr_list, dtype=torch.float32) if tr_list else None
+            va = torch.tensor(va_list, dtype=torch.float32) if va_list else None
+            return tr, va
         
         else:
-            raise ValueError(f"Aggregation method {aggregation} is not supported.")
+            raise ValueError(
+                f"Aggregation method {aggregation} is not supported.")
